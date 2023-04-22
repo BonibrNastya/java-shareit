613bdc267256d55a87a8cb63353da3aefcdf41b1
Анастасия <anime145@yandex.ru>
2023-04-22 13:24:53 +0300

    фз-13.2.CS1

--- src/main/java/ru/practicum/shareit/request/storage/ItemRequestStorage.java
+++ src/main/java/ru/practicum/shareit/request/storage/ItemRequestStorage.java
@@ -3,6 +3,6 @@ package ru.practicum.shareit.request.storage;
 import ru.practicum.shareit.request.ItemRequest;
 
 public interface ItemRequestStorage {
-    ItemRequest getById (long id);
+    ItemRequest getById(long id);
 
 }
